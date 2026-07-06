5cae9660d6fc6662e945cc05f5254479cbc4960e
APP <Abolfazlpakdelpour@gmail.com>
2026-07-06 14:45:10 +0330

    end of season 2
    until راه های مختلف گرفتن مدلاسیون USER

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,74 @@
 from django.db import models
-
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+# =============
+from django.utils.translation import gettext_lazy as _
+# =============
 # Create your models here.
+class UserManager(BaseUserManager):
+    '''
+    Custom user manager that provides `create_user` and `create_superuser`
+    '''
+    def create_user(self, email, password, **extra_fields):
+        '''
+        Creates and saves a new user with given email and password.
+        '''
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    def create_superuser(self, email, password, **extra_fields):
+        '''
+        Creates and saves a new super user with given email and password.
+        '''
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''
+    custom user model for our app
+    '''
+    email = models.EmailField(max_length=255, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=255)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+    def __str__(self):
+        return self.email
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
