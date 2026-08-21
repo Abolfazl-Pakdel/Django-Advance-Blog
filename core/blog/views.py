cddded6644623d0ad7bfd1d31dc149a6f6091025
APP <Abolfazlpakdelpour@gmail.com>
2026-08-21 14:26:01 +0330

    this is until sesen 14

--- core/blog/views.py
+++ core/blog/views.py
@@ -104,3 +104,6 @@ class PostEditView(LoginRequiredMixin, UpdateView):
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = "/blog/post"
+
+class PostListApiView(TemplateView):
+    template_name = 'blog/post_list_api.html'
